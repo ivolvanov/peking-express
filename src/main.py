1820cc4faa0138320d4975da1d7c831814bd67da
Aleksnader.G <425730@student.fontys.nl>
2021-03-20 11:49:30 +0100

    algorithm for generating all possible paths to the destination node added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import json
+from collections import defaultdict 
 
+
+#paths = []
 class PekingGame(object):
     map_dict = {}
     startLocation = 0
@@ -37,7 +40,66 @@ class PekingGame(object):
         self.competitorLocations[groupid - 1] = location
 
     def nextMove(self) -> int:
+
         return 0
+        
+class Graph: 
+   
+    def __init__(self, vertices): 
+
+        self.V = vertices  
+        
+        self.graph = defaultdict(list) 
+
+        self.paths = list()
+ 
+    def addEdge(self, u, v): 
+        self.graph[u].append(v) 
+
+    def pathsUtil(self, u, d, visited, path): 
+
+        visited[u]= True
+        path.append(u) 
+  
+        if u == d:
+            currPath = path[:]
+            self.paths.append(currPath)
+            print(path)
+        else: 
+            for i in self.graph[u]: 
+                if visited[i]== False: 
+                    self.pathsUtil(i, d, visited, path) 
+                      
+        path.pop() 
+        visited[u] = False
+   
+    def generatePaths(self, s, d): 
+
+        visited =[False]*(self.V) 
+
+        path = []
+
+        self.pathsUtil(s, d, visited, path) 
+
+
+   
+   
+   
+# Create a graph given in the above diagram 
+g = Graph(4) 
+g.addEdge(0, 1) 
+g.addEdge(0, 2) 
+g.addEdge(0, 3) 
+g.addEdge(2, 0) 
+g.addEdge(2, 1) 
+g.addEdge(1, 3) 
+   
+s = 2 ; d = 3
+print ("Following are all different paths from % d to % d :" %(s, d)) 
+g.generatePaths(s, d) 
+print(g.paths)
+
+# This code is contributed by Neelam Yadav
 
 
 # if __name__ == '__main__':
