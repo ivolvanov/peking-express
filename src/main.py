3313bc2479bdce6d881c8052454fde98c66f5929
Aleksnader.G <425730@student.fontys.nl>
2021-03-20 12:41:34 +0100

    parsed the data from the testfile

--- src/main.py
+++ src/main.py
@@ -12,17 +12,29 @@ class PekingGame(object):
     sources = []
     targets = []
     prices = []
-    competitorLocations = [0] * 9
+    occupiedLocations = []
+    turn = 0
+    budget = 0
 
-    def __init__(self, map, startLocation):
-        self.map = map
-        self.setStartLocation(startLocation) 
-        self.currentLocation = startLocation  
-        #self.initializeMap(map) 
-
+    # constuctor
+    # initializes the initial state of the game
+    def __init__(self, testfileLocation):
+        testfile = []
+        f = open(testfileLocation, "r")
+        for line in f:
+            testfile.append(line.strip())
+        f.close()
+        map = json.loads(testfile[0])
+        startLocation = json.loads(testfile[1])
+        budget = json.loads(testfile[2])
+        occupiedLocations = json.loads(testfile[3])
+   
+        self.initializeMap(map)
+        self.setStartLocation(startLocation)
+        self.updateOccupiedLocations(occupiedLocations , self.turn)
+        self.setBudget(budget)
 
     def initializeMap(self, map):
-        #self.map_dict = json.load(map)
         self.map_dict = map
         self.nrOfLocations = self.map_dict['locations']['number']
         self.criticalLocations = self.map_dict['locations']['critical']
@@ -34,13 +46,22 @@ class PekingGame(object):
     def setStartLocation(self, startLocation_param : int):
         self.startLocation = startLocation_param
 
-    # IMPORTANTE - the competitorLocations list stores the location of group n at index n-1
-    # i.e. the location of group 3 is at competitorLocations[2]
-    def updateCompetitorLocation(self, groupid : int, location : int):
-        self.competitorLocations[groupid - 1] = location
+    def setBudget(self,budget_param : int):
+        self.budget = budget_param
+
+    def updateTurnCount(self):
+        self.turn +=1
+
+    # updates the occupiedLocations per turn
+    def updateOccupiedLocations(self,locationList , turn : int ):
+        #self.competitorLocations[groupid - 1] = location
+        self.occupiedLocations.append(locationList)
 
     def nextMove(self) -> int:
 
+        #algorithm goes BRRRRRRRRRRR
+
+        updateTurnCount()
         return 0
         
 class Graph: 
@@ -81,10 +102,6 @@ class Graph:
 
         self.pathsUtil(s, d, visited, path) 
 
-
-   
-   
-   
 # Create a graph given in the above diagram 
 g = Graph(4) 
 g.addEdge(0, 1) 
@@ -94,10 +111,11 @@ g.addEdge(2, 0)
 g.addEdge(2, 1) 
 g.addEdge(1, 3) 
    
-s = 2 ; d = 3
-print ("Following are all different paths from % d to % d :" %(s, d)) 
-g.generatePaths(s, d) 
-print(g.paths)
+#s = 2 ; d = 3
+#print ("Following are all different paths from % d to % d :" %(s, d)) 
+#g.generatePaths(s, d) 
+#print(g.paths)
+game = PekingGame("test1.txt")
 
 # This code is contributed by Neelam Yadav
 
